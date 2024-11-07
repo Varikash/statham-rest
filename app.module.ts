5f6f47957de542bdffe55da24c3fa607d0f29f9e
Nikolay Varikash <n.varicash@gmail.com>
2024-11-07 02:27:06 +0200

    structure fix

--- app.module.ts
+++ app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { QuotesModule } from './quotes/quotes.module';
+import { QuotesModule } from './src/quotes/quotes.module';
 
 @Module({
   imports: [
